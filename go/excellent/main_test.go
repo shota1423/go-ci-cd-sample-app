c15c0cc2e54d66a7766e1d003a92868019624022
shota1423 <98408731+shota1423@users.noreply.github.com>
2025-10-31 14:50:09 +0900

    odd

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
-	result := EvenOrOdd(10)
+	result := EvenOrOdd(9)
 	if result != "even" {
 		t.Errorf("Expected even, actual %s", result)
 	}
