d827b7d86152cd295cef80cdc3c01d479739d4f0
shota1423 <98408731+shota1423@users.noreply.github.com>
2025-11-02 13:25:06 +0900

    テストケースの入力値を修正して、奇数のテストを行うように変更

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
-	result := EvenOrOdd(10)
+	result := EvenOrOdd(3)
 	if result != "even" {
 		t.Errorf("Expected even, actual %s", result)
 	}
